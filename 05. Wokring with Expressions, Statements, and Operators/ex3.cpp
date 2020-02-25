a20cb30d046ca6f603ec1d7f55df5c51405cbab6
hpark95 <parkhs1995@gmail.com>
2020-02-25 14:08:48 -0500

    feat: lesson 5 reviewed

--- 05. Wokring with Expressions, Statements, and Operators/ex3.cpp
+++ 05. Wokring with Expressions, Statements, and Operators/ex3.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main() {
-    cout << "Enter a boolean value true(1) or false(0): ";
+    cout << "Enter a boolean value 0 or 1 for value1: ";
     bool value1 = false;
     cin >> value1;
 
-    cout << "Enter another boolean value true(1) or false(0): ";
+    cout << "Enter another boolean value 0 or 1 for value2: ";
     bool value2 = false;
     cin >> value2;
 
-    cout << "Result of bitwise operatons on these operands: " << endl;
-    cout << "Bitwise AND: " << (value1 & value2) << endl;
-    cout << "Bitwise OR: " << (value1 | value2) << endl;
-    cout << "Bitwise XOR: " << (value1 ^ value2) << endl;
+    cout << "Result of AND, OR, and XOR on value1 and value2: " << endl;
+    cout << "value1 & value2: " << (value1 & value2) << endl;
+    cout << "alue1 | value2: " << (value1 | value2) << endl;
+    cout << "value1 ^ value2: " << (value1 ^ value2) << endl;
     
     return 0;
 }
